1265d234cd9c6b5ab0f5c40b068b9d92ff0eca5f
Afroz <Sunkesula.afroz@ametek.com>
2025-04-04 17:11:56 +0530

    ASP.NET 8 MVC Initial Project Created

---
+++ Once Upon A Book/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Once_Upon_A_Book.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
